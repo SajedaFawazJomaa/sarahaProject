b345d00197d1a1cb6ec3430a4718ee4c9b21fb95
SajedaFawazJomaa <155645043+SajedaFawazJomaa@users.noreply.github.com>
2024-01-04 16:13:25 +0200

    Create Message.model.js

---
+++ DB/Models/Message.model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema, model, Types} from 'mongoose';
+
+
+const MessageSchema = new Schema({
+    messages:{
+        type:String,
+        required : true,
+    },
+    receiverId:{
+        type:Types.ObjectId,
+        ref:'User',
+        required:true,
+    }
+},{
+    timestamps:true
+});
+
+const MessageModel = mongoose.models.Message || model('Message',MessageSchema)
+export default MessageModel;
