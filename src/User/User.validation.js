2cff0f2db26ad44f43630ac5136a8b5438c3f469
SajedaFawazJomaa <155645043+SajedaFawazJomaa@users.noreply.github.com>
2024-01-04 16:21:48 +0200

    Create User.validation.js

---
+++ src/User/User.validation.js
@@ -0,0 +1,25 @@
+
+import { generalFields } from '../../middleware/validation.js';
+import joi  from 'joi';
+
+
+export const profile = {
+    file:generalFields.file.required(),
+}
+
+export const updatePassword = {
+    body:joi.object({
+        oldPassword:generalFields.password,
+        newPassword:generalFields.password.invalid(joi.ref('oldPassword')),
+        cpassword:joi.string().valid(joi.ref('newPassword')).required(),
+
+
+    })
+   
+};  
+
+export const shareProfile = {
+    params:joi.object({
+        id:generalFields.id,
+    })
+}
